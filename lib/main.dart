c0da81173a691342520ed836b6c86f1a51f5fd28
MasterUNG <phrombutr@gmail.com>
2018-07-15 14:14:42 +0700

    Update 15July

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,18 @@
 import 'package:flutter/material.dart';
 
-void main()
- {
+void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-
     return new MaterialApp(
       title: "GitView Multiple Image",
       home: new Scaffold(
-        appBar: new AppBar(title: new Text("App Bar Test"),),
+        appBar: new AppBar(
+          title: new Text("App Bar Test"),
+        ),
         body: new MyStatefulWidget(),
       ),
     );
@@ -20,7 +20,6 @@ class MyApp extends StatelessWidget {
 }
 
 class MyStatefulWidget extends StatefulWidget {
-
   @override
   State<StatefulWidget> createState() {
     // TODO: implement createState
@@ -36,7 +35,7 @@ class _MyStatefulWidgetState extends State<MyStatefulWidget> {
         title: new Text("GritView Multiple Image"),
       ),
       body: new GridView.extent(
-          maxCrossAxisExtent: 150.0,
+        maxCrossAxisExtent: 150.0,
         mainAxisSpacing: 5.0,
         crossAxisSpacing: 5.0,
         padding: EdgeInsets.all(5.0),
@@ -47,15 +46,35 @@ class _MyStatefulWidgetState extends State<MyStatefulWidget> {
 }
 
 List<Widget> _buildGridTitles(numberOfTiles) {
-  List<Container> listContainner = new List<Container>.generate(numberOfTiles,
-  (int index){
+  List<Stack> listContainner =
+      new List<Stack>.generate(numberOfTiles, (int index) {
     final imageName = 'images/master${index + 1}.jpg';
-
-      return new Container(
-        child: new Image.asset(imageName,
-          fit: BoxFit.cover,),
-      );
-  }
-  );
+    return new Stack(
+      alignment: const Alignment(1.0, 1.0),
+      children: <Widget>[
+        new Container(
+          child: new Image.asset(
+            imageName,
+            width: 150.0,
+            height: 150.0,
+            fit: BoxFit.cover,
+          ),
+        ),
+        new Container(
+        padding: EdgeInsets.all(10.0),
+        decoration: new BoxDecoration(
+            color: new Color.fromARGB(120, 0, 0, 0)
+        ),
+          child: new Text(
+            'Master${index}',
+            style: new TextStyle(
+                fontSize: 20.0,
+                fontWeight: FontWeight.bold,
+                color: Colors.white),
+          ),
+        )
+      ],
+    );
+  }); //index
   return listContainner;
-}
+} // List
