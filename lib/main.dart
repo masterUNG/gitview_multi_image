de4da781ca2c17b40a2993a84fcf78e8b6aa606e
MasterUNG <phrombutr@gmail.com>
2018-07-15 08:22:49 +0700

    Project Success

---
+++ lib/main.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+
+void main()
+ {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+
+    return new MaterialApp(
+      title: "GitView Multiple Image",
+      home: new Scaffold(
+        appBar: new AppBar(title: new Text("App Bar Test"),),
+        body: new MyStatefulWidget(),
+      ),
+    );
+  }
+}
+
+class MyStatefulWidget extends StatefulWidget {
+
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return new _MyStatefulWidgetState();
+  }
+}
+
+class _MyStatefulWidgetState extends State<MyStatefulWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      appBar: new AppBar(
+        title: new Text("GritView Multiple Image"),
+      ),
+      body: new GridView.extent(
+          maxCrossAxisExtent: 150.0,
+        children: _buildGridTitles(30),
+      ),
+    );
+  }
+}
+
+List<Widget> _buildGridTitles(numberOfTiles) {
+  List<Container> listContainner = new List<Container>.generate(numberOfTiles,
+  (int index){
+    final imageName = 'images/master${index + 1}.jpg';
+      return new Container(
+        child: new Image.asset(imageName),
+      );
+  }
+  );
+  return listContainner;
+}
